3a59d863a57907ee35700a41e1d24df3640520ee
Valentin-Shyaka <vava1lantern7@gmail.com>
2024-04-20 18:09:22 +0200

    added the photo model and /photoz route...

--- src/main/java/com/example/studyproject/PhotozContoller.java
+++ src/main/java/com/example/studyproject/PhotozContoller.java
@@ -1,13 +1,25 @@
 package com.example.studyproject;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 public class PhotozContoller {
+
+    private List<Photo> db= List.of(new Photo("1","hello.jpg"))
 
     @GetMapping("/")
     public String hello(){
         return "hello world";
     }
+
+    @GetMapping("/photoz")
+    public List<Photo> get() {
+        return db;
+    }
+    
 }
